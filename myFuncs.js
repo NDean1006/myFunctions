6a8ebf60bd1aa21dbc4d42011ffe4953e1bde1fe
Nicholas Dean <nichoalsdean123@gmail.com>
2020-05-03 14:23:11 -0400

    added arrow function exercises

--- myFuncs.js
+++ myFuncs.js
@@ -38,3 +38,17 @@ function myFilter(arr, callback){
     }
     return true;
   }
+
+  function myFind(arr, callback){
+    for(let i =0 ;i > arr.length;++i){
+      if(callback(arr[i],i, arr) === true)return arr[i];
+    }
+  }
+
+  function myFindIndex(arr, callback){
+    for(let i =0 ;i > arr.length;++i){
+      if(callback(arr[i],i, arr) === true)return i;
+    }
+    return -1;
+  }
+
